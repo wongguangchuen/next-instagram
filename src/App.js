5c600fc31601a077ae5674e551bb785ac6cc9188
Guang Chuen <gc@guangs-mbp.lan>
2020-12-14 09:55:04 +0800

    commitment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState ,useEffect} from 'react'
+import axios from 'axios'
+import LoadingIndicator from './components/LoadingIndicator'
+import HomePage from './pages/HomePage'
+import UserProfilePage from './pages/UserProfilePage'
+import { Route} from "react-router-dom"
+import NavBarDisplay from './components/NavBarDisplay';
+import { ToastContainer } from 'react-toastify';
+import MyProfilePage from './pages/MyProfilePage'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  const [loggedIn, setLoggedIn] = useState(
+    localStorage.getItem('jwt') !== null
+  )
+
+  const [users, setUsers] = useState([])
+  const [ isloading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    //perform a GET request
+    axios.get('https://insta.nextacademy.com/api/v1/users')
+    .then(result => {
+      // If successful, we do stuffs with 'result'
+      // console.log(result.data)
+      setUsers(result.data)
+      setIsLoading(false)
+    })
+    .catch(error => {
+      // If unsuccessful, we notify users what went wrong
+      console.log(error)
+      console.log('ERROR: ', error)
+    })
+
+  }, [])
+
+  if(isloading){
+    return <LoadingIndicator width="150px" height="150px" color="green" /> 
+  }
+ 
+  return(
+    <>
+        <NavBarDisplay loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>    
+        <Route exact path="/" render = {(props) => <HomePage{...props} users={users}/>}/>
+          
+        <Route path="/users/:id" component={UserProfilePage} /> 
+        <Route exact path="/profile" component={MyProfilePage} />
+        <ToastContainer />
+       
+    </>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
